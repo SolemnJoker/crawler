af7d4ae2080d19ce82866f743fd77f032e2aeffc
SolemnJoker <wenxiongkjsm@163.com>
2017-07-13 14:42:15 +0800

    baiduimage json version

--- baiduImage.py
+++ baiduImage.py
@@ -1,60 +1,67 @@
 # coding:UTF-8
+import time
+import os
 from bs4 import BeautifulSoup
 import requests
-import time
 url = "http://image.baidu.com/search/acjson"
-path = "./"
+path = 'E:/personal/jiepai'
 
 
 def getUrl(url, curParams=None):
     try:
         r = requests.get(url, params=curParams)
-        r.raise_for_status()
-        r.encoding = r.apparent_encoding
-        return r.text
+        return r
     except:
         return ""
 
 
 def download(url, filename):
-    ir = requests.get(url)
-    if ir.status_code == 200:
-        filePathName = os.path.json(path, filename)
-        open(filePathName, 'wb').write(ir.content)
+    try:
+        ir = requests.get(url)
+        ir.raise_for_status()
+        if ir.status_code == 200:
+            filePathName = os.path.join(path, filename)
+            open(filePathName, 'wb').write(ir.content)
+    except BaseException,e:
+        print 'download error :%s'%filename
+        print e.message
 
-
-def requests(params):
-    response = requests.get(url, params=params)
-    print response
-    json = response.json()['imgs']
-
-    for i in range(0, len(json)):
-        filename = os.path.split(json[i]['objURL'])[1]
-        print 'Downloading from %s' % json[i]['objURL']
-        download(json[i]['objURL'], filename)
-
+def request(params):
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        json = response.json()['data']
+        for i in range(0, len(json)):
+            if json[i].has_key('replaceUrl') :
+                objUrl = json[i]['replaceUrl'][-1]['ObjURL']
+                filename = os.path.split(objUrl)[1].split('?')[0]
+                if(len(filename) != 0):
+                    fromHost = json[i]['fromURLHost']
+                    print 'Downloading from %s' % objUrl
+                    download(objUrl, filename)
+    except:
+        return "get url error"
 
 def search(keyword, minpage, maxpage):
     params = {
-        'tn': 'resultjsonavatarnew',
+        'tn': 'resultjson_com',
+        'word': keyword,
+        'queryWord':keyword,
         'ie': 'utf-8',
         'cg': '',
-        'itg': '',
-        'z': '0',
-        'fr': '',
-        'width': '',
-        'height': '',
-        'lm': '-1',
-        'ic': '0',
-        's': '0',
-        'word': keyword,
-        'st': '-1',
-        'gsm': '1e',
-        'rn': '30'
+        'ct':'201326592',
+        'fp':'result',
+        'cl':'2',
+        'lm':'-1',
+        'rn': '30',
+        'ipn':'rj'
     };
     for i in range(minpage, maxpage):
+        print 'Download page %d:'%i 
         params['pn'] = '%d' % (i * 30)
-        requests(params)
+        request(params)
 
-path = './jiepai'
-search('街拍', 1, 10)
+def start():
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+    search('街拍', 1, 10)
